13b23ebe179a9c6134002b49319e830f6f9fb9c0
Firefly4266 <gaines.johnp@gmail.com>
2017-08-19 17:11:36 -0700

    Added files

---
+++ MultipleMethods/MultipleMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class MultipleMethods
+{
+    public static void Main(String[] args)
+    {
+        InputWrapper iw = new InputWrapper();
+        int [] primes = {2, 3, 5, 7, 11, 13 };
+        for (int i = 0; i < primes.Length; i++)
+        {
+            Console.Write("{0} ", primes[i]);
+        }
+            Console.WriteLine();
+    }
+}
